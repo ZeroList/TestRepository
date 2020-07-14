d786714b4f82feb6a2541a6840a75de6ad7d81a6
LH <helloSIx@itsx.com>
2020-07-15 02:56:54 +0800

    IDEA上传GitHub测试

--- boot_mq_topic_produce/src/main/java/com/itsx/activemq/config/ConfigBean.java
+++ boot_mq_topic_produce/src/main/java/com/itsx/activemq/config/ConfigBean.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component      // 让spring 管理的注解，相当于spring 中在xml 中写了个bean
-@EnableJms      // 开启jms 适配
+@EnableJms      // 开启jms    适配
 public class ConfigBean {
     @Value("${mytopic}")     // 注入配置文件中的 myqueue
     private String myTopic;
